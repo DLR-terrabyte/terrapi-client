ee3123589cfbd077efa3009d653cf0fbaf59d3ce
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2024-02-10 00:39:36 +0100

    Cleanup. Fixing functions to modify collections/items

--- src/terrabyte/client.py
+++ src/terrabyte/client.py
@@ -5,46 +5,54 @@ import pystac
 import pystac_client
 import requests
 
-from terrabyte_auth.adapter import create_requests_adapter
+from terrabyte_auth.adapter import create_requests_adapter, wrap_request
 
 from .settings import (
     TERRABYTE_PRIVATE_API_URL,
     TERRABYTE_PUBLIC_API_URL,
-    TERRABYTE_AUTH_URL,
-    TERRABYTE_CLIENT_ID,
 )
 
 
-def open_private_catalog(token: Optional[str] = None) -> pystac_client.Client:
+def open_private_catalog() -> pystac_client.Client:
     return pystac_client.Client.open(
         url=TERRABYTE_PRIVATE_API_URL,
-        request_modifier=create_requests_adapter(
-            TERRABYTE_CLIENT_ID, TERRABYTE_AUTH_URL
-        ),
+        request_modifier=create_requests_adapter(),
     )
+
+
+def _send_client_request(client: pystac_client.Client, url: str, *args, **kwargs):
+    return wrap_request(client._stac_io.session, url, *args, **kwargs)
 
 
 def create_private_collection(
     client: pystac_client.Client, collection: pystac.Collection
 ):
-    client._stac_io.session.post(
-        f"{TERRABYTE_PRIVATE_API_URL}/collections", json=collection.to_dict()
+    _send_client_request(
+        client,
+        f"{TERRABYTE_PRIVATE_API_URL}/collections",
+        method="POST",
+        json=collection.to_dict(),
     ).raise_for_status()
 
 
 def update_private_collection(
     client: pystac_client.Client, collection: pystac.Collection
 ):
-    client._stac_io.session.put(
-        f"{TERRABYTE_PRIVATE_API_URL}/collections", json=collection.to_dict()
+    _send_client_request(
+        client,
+        f"{TERRABYTE_PRIVATE_API_URL}/collections",
+        method="PUT",
+        json=collection.to_dict(),
     ).raise_for_status()
 
 
 def delete_private_collection(
     client: pystac_client.Client, collection: pystac.Collection
 ):
-    client._stac_io.session.delete(
-        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection.id}"
+    _send_client_request(
+        client,
+        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection.id}",
+        method="DELETE",
     ).raise_for_status()
 
 
@@ -54,8 +62,12 @@ def create_private_item(
     collection_id = collection_id or item.collection_id
     if not collection_id:
         raise ValueError(f"Could not determine collection ID for item {item}")
-    client._stac_io.session.post(
-        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection_id}", json=item.to_dict()
+
+    _send_client_request(
+        client,
+        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection_id}",
+        method="POST",
+        json=item.to_dict(),
     ).raise_for_status()
 
 
@@ -65,8 +77,12 @@ def update_private_item(
     collection_id = collection_id or item.collection_id
     if not collection_id:
         raise ValueError(f"Could not determine collection ID for item {item}")
-    client._stac_io.session.put(
-        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection_id}/items/{item.id}", json=item.to_dict()
+
+    _send_client_request(
+        client,
+        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection_id}/items/{item.id}",
+        method="PUT",
+        json=item.to_dict(),
     ).raise_for_status()
 
 
@@ -76,8 +92,11 @@ def delete_private_item(
     collection_id = collection_id or item.collection_id
     if not collection_id:
         raise ValueError(f"Could not determine collection ID for item {item}")
-    client._stac_io.session.delete(
-        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection_id}/items/{item.id}"
+
+    _send_client_request(
+        client,
+        f"{TERRABYTE_PRIVATE_API_URL}/collections/{collection_id}/items/{item.id}",
+        method="DELETE",
     ).raise_for_status()
 
 
--- src/terrabyte/settings.py
+++ src/terrabyte/settings.py
@@ -11,12 +11,3 @@ TERRABYTE_PRIVATE_API_URL = os.environ.get(
     # "https://gateway.terrabyte.eox.at/private/stac",
     "https://terrabyte-api.hub.eox.at/private/stac"
 )
-
-TERRABYTE_AUTH_URL = os.environ.get(
-    "TERRABYTE_AUTH_URL",
-    "https://auth.terrabyte.lrz.de/realms/terrabyte/",
-)
-TERRABYTE_CLIENT_ID = os.environ.get(
-    "TERRABYTE_CLIENT_ID",
-    "at.eox.hub.terrabyte-api",
-)
