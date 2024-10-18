49c04f639e42de0b158498a4a010b3ad89ca7888
Jonas Eberle <jonas.eberle@gmx.de>
2024-10-18 14:20:12 +0200

    Change API URLs to terrabyte STAC

--- src/terrabyte/settings.py
+++ src/terrabyte/settings.py
@@ -3,11 +3,9 @@ import os
 
 TERRABYTE_PUBLIC_API_URL = os.environ.get(
     "TERRABYTE_PUBLIC_API_URL",
-    # "https://gateway.terrabyte.eox.at/public/stac",
-    "https://terrabyte-api.hub.eox.at/public/stac"
+    "https://stac.terrabyte.lrz.de/public/api"
 )
 TERRABYTE_PRIVATE_API_URL = os.environ.get(
     "TERRABYTE_PRIVATE_API_URL",
-    # "https://gateway.terrabyte.eox.at/private/stac",
-    "https://terrabyte-api.hub.eox.at/private/stac"
+    "https://stac.terrabyte.lrz.de/private/api"
 )
